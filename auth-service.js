44aa8350fda1701f779f8230b3c4d3140c25d96d
Vu Duc Thuan Tran <vdtr2808@gmail.com>
2023-12-01 01:02:04 -0500

    initial message

--- auth-service.js
+++ auth-service.js
@@ -1,101 +1,95 @@
-const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const dotenv = require('dotenv');
 
-
-dotenv.config();
-
-const MONGO_USER = process.env.MONGO_USER;
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
-const MONGO_CLUSTER = process.env.MONGO_CLUSTER;
-const MONGO_DATABASE = process.env.MONGO_DATABASE;
-
-const userSchema = new mongoose.Schema({
-    userName: {
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+    "userName": {
         type: String,
-        unique: true,
+        required: true
     },
-    password: String,
-    email: String,
-    loginHistory: {
-        type: [{
-            dateTime: Date,
-            userAgent: String,
-        }],
-        default: [],
-    },
+    "password": String,
+    "email": String,
+    "loginHistory": [{
+        "dateTime": Date,
+        "userAgent": String
+    }]
 });
 
 let User;
 
-initialize = () => {
+initialize = function () {
     return new Promise(function (resolve, reject) {
-        const uri = `mongodb+srv://vdtr2808:vwbkDdhvjNkpRcYL@cluster0.qbgnaqa.mongodb.net/?retryWrites=true&w=majority`;
-        
-        let db = mongoose.createConnection(uri);
+        let db = mongoose.createConnection("mongodb+srv://vdtr2808:vwbkDdhvjNkpRcYL@cluster0.qbgnaqa.mongodb.net/");
 
-        db.on('error', (err) => {
+        db.on('error', (err)=>{
             reject(err); // reject the promise with the provided error
         });
-        db.once('open', () => {
-            User = db.model("users", userSchema);
-            resolve();
+        db.once('open', ()=>{
+           User = db.model("users", userSchema);
+           resolve();
         });
     });
 };
+
+
 function registerUser(userData) {
-    return new Promise((resolve, reject) => {
-        if (userData.password !== userData.password2) reject('Passwords do not match!');
-        else {
-            bcrypt.hash(userData.password, 10) 
-                .then(hash => {
-                    let newUser = new User({
-                        userName: userData.userName,
-                        password: hash, 
-                        email: userData.email,
-                        loginHistory: [] 
-                    });
-                    newUser.save()
-                        .then(() => resolve())
-                        .catch(err => {
-                            if (err.code === 11000) reject("Username already taken");
-                            else reject('There was an error creating the user: ${err}');
-                        });
-                })
+    return new Promise(function (resolve, reject) {
+        if (userData.password !== userData.password2) {
+            reject("Passwords do not match");
+        } else {
+            bcrypt
+            .hash(userData.password, 10)
+            .then((hash) => {
+                userData.password = hash;
+                let user = new User(userData);
+                user.save()
+                .then(() => resolve())
                 .catch(err => {
-                    console.log(err); 
-                    reject('There was an error encrypting the password');
-                });
+                    err.code === 11000 
+                        ? reject("User Name already taken")
+                        : reject(`There was an error creating the user: ${err}`);
+
+                })
+            })
         }
     });
-}
+};
 
 function checkUser(userData) {
-    return new Promise((resolve, reject) => {
-        User.findOne({ userName: userData.userName }).exec()
-            .then(user => {
-                if (!user) reject('Incorrect username');
-                else {
-                    bcrypt.compare(userData.password, user.password).then(result => {
-                        if (result) {
-                            user.loginHistory.push({
-                                dateTime: new Date(),
-                                userAgent: userData.userAgent
-                            });
-                            User.updateOne({ userName: user.userName }, { $set: { loginHistory: user.loginHistory } })
-                                .then(() => resolve(user))
-                                .catch(newErr => reject('There was an error updating the user login history: ${newErr}'));
-                        } 
-                        else reject('Incorrect Password for user: ${userData.userName}');
-                    });
-                }
-            })
-            .catch(error => { reject('Unable to find user: ${userData.userName}'); });
+    let user = userData.userName;
+    return new Promise(function (resolve, reject) {
+        // User.find().then(users => console.log(users))
+        User.find({ userName: user })
+        .exec()
+        .then(users => {
+            if (users.length < 1) {
+                reject(`Unable to find user: ${user}`);
+            } else {
+                bcrypt
+                .compare(userData.password, users[0].password)
+                .then((result) => {
+                    if (!result)
+                        reject(`Incorrect Password for user: ${user}`);
+                    else {
+                        users[0].loginHistory.push({
+                            dateTime: new Date().toString(),
+                            userAgent: userData.userAgent
+                        });
+                        User.updateOne(
+                            { userName: users[0].userName },
+                            { $set: { loginHistory: users[0].loginHistory } })
+                            .then(() => resolve(users[0]))
+                            .catch(err => reject(`There was an error verifying the user: ${err}`));
+                    }
+                }).catch(err => reject(`Unable to find user: ${user}`))
+            }
+        })
     });
-}
+};
+
 
 module.exports = {
     initialize,
     registerUser,
-    checkUser,
-}
+    checkUser
+};
