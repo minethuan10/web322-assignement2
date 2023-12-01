967afea5d8261528f6e17f44bb53c6d87edcb25d
Vu Duc Thuan Tran <vdtr2808@gmail.com>
2023-12-01 00:11:01 -0500

    initial message

--- auth-service.js
+++ auth-service.js
@@ -44,62 +44,55 @@ initialize = () => {
     });
 };
 function registerUser(userData) {
-    return new Promise(function (resolve, reject) {
-        if (userData.password !== userData.password2) {
-            reject("Passwords do not match");
-        } else {
-            bcrypt
-            .hash(userData.password, 10)
-            .then((hash) => {
-                userData.password = hash;
-
-                const newUser = new User(userData); // Create a new user instance
-                newUser.save()
-                .then(() => resolve())
+    return new Promise((resolve, reject) => {
+        if (userData.password !== userData.password2) reject('Passwords do not match!');
+        else {
+            bcrypt.hash(userData.password, 10) 
+                .then(hash => {
+                    let newUser = new User({
+                        userName: userData.userName,
+                        password: hash, 
+                        email: userData.email,
+                        loginHistory: [] 
+                    });
+                    newUser.save()
+                        .then(() => resolve())
+                        .catch(err => {
+                            if (err.code === 11000) reject("Username already taken");
+                            else reject('There was an error creating the user: ${err}');
+                        });
+                })
                 .catch(err => {
-                    err.code === 11000 
-                        ? reject("User Name already taken")
-                        : reject(`There was an error creating the user: ${err}`);
+                    console.log(err); 
+                    reject('There was an error encrypting the password');
                 });
-            })
-            .catch(err => reject(`Error hashing password: ${err}`));
         }
     });
-};
-
+}
 
 function checkUser(userData) {
-    let user = userData.userName;
-    return new Promise(function (resolve, reject) {
-        // User.find().then(users => console.log(users))
-        User.find({ userName: user })
-        .exec()
-        .then(users => {
-            if (users.length < 1) {
-                reject(`Unable to find user: ${user}`);
-            } else {
-                bcrypt
-                .compare(userData.password, users[0].password)
-                .then((result) => {
-                    if (!result)
-                        reject(`Incorrect Password for user: ${user}`);
-                    else {
-                        users[0].loginHistory.push({
-                            dateTime: new Date().toString(),
-                            userAgent: userData.userAgent
-                        });
-                        User.updateOne(
-                            { userName: users[0].userName },
-                            { $set: { loginHistory: users[0].loginHistory } })
-                            .then(() => resolve(users[0]))
-                            .catch(err => reject(`There was an error verifying the user: ${err}`));
-                    }
-                }).catch(err => reject(`Unable to find user: ${user}`))
-            }
-        })
+    return new Promise((resolve, reject) => {
+        User.findOne({ userName: userData.userName }).exec()
+            .then(user => {
+                if (!user) reject('Incorrect username');
+                else {
+                    bcrypt.compare(userData.password, user.password).then(result => {
+                        if (result) {
+                            user.loginHistory.push({
+                                dateTime: new Date(),
+                                userAgent: userData.userAgent
+                            });
+                            User.updateOne({ userName: user.userName }, { $set: { loginHistory: user.loginHistory } })
+                                .then(() => resolve(user))
+                                .catch(newErr => reject('There was an error updating the user login history: ${newErr}'));
+                        } 
+                        else reject('Incorrect Password for user: ${userData.userName}');
+                    });
+                }
+            })
+            .catch(error => { reject('Unable to find user: ${userData.userName}'); });
     });
-};
-
+}
 
 module.exports = {
     initialize,
